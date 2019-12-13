8c05ebf5de9317d7d58bc2d6967013629df0b9d4
annsilich <53449667+annsilich@users.noreply.github.com>
2019-12-13 22:34:51 +0300

    Add files via upload

---
+++ task2.lisp
@@ -0,0 +1,8 @@
+;; Задание 2
+;; Чтение файла и вывод его содержимого (файл текстовый)
+
+(let ((in (open "task2.txt" :if-does-not-exist nil)))
+    (when in
+        (loop for line = (read-line in nil)
+    while line do (format t "~a~%" line))
+    (close in)))
