b7b93b34069c7d00acf415ca176ab82a676f73f8
annsilich <53449667+annsilich@users.noreply.github.com>
2019-12-23 15:05:49 +0300

    Update task2.lisp

--- task2.lisp
+++ task2.lisp
@@ -1,8 +1,12 @@
 ;; Задание 2
 ;; Чтение файла и вывод его содержимого (файл task2.txt)
 
-(let ((in (open "task2.txt" :if-does-not-exist nil)))
-    (when in
-        (loop for line = (read-line in nil)
-    while line do (format t "~a~%" line))
-    (close in)))
+(defun readingfile (textfile)
+    (let ((in (open textfile :if-does-not-exist nil)))
+        (when in
+            (loop for line = (read-line in nil)
+                while line do (format t "~a~%" line))
+        (close in)))
+)
+
+(readingfile "task2.txt")
